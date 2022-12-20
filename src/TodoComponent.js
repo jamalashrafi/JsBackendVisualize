48b035c969e410a5c7a4440138dc17c823716b9e
jamalashrafi <jamalnabi07@gmail.com>
2022-12-20 23:42:45 +0530

    add local storage

--- src/TodoComponent.js
+++ src/TodoComponent.js
@@ -4,7 +4,10 @@ exports.TodoComponent = void 0;
 const TodoModal_1 = require("./TodoModal");
 class TodoComponent {
     constructor(selector) {
+        var _a;
         this.todoList = [];
+        //* Gets Todos from Local storage
+        this.todoList = JSON.parse((_a = localStorage.getItem('Todos')) !== null && _a !== void 0 ? _a : "[]");
         const template = `
         <div>
             <input type="text" id="todo-input" placeholder="Enter text here" />
@@ -18,10 +21,15 @@ class TodoComponent {
         const addTodo = document.querySelector("#todo-button");
         const itemList = document.querySelector("#item-list");
         const todoInput = document.querySelector("#todo-input");
+        //- Render the todoList on Dom
+        this.todoList.forEach((todo) => {
+            const item = todo.todoItem;
+            itemList.insertAdjacentHTML('beforeend', `<li>${item}</li>`);
+        });
         addTodo.addEventListener("click", () => {
             const todoInputValue = new TodoModal_1.TodoModal(todoInput === null || todoInput === void 0 ? void 0 : todoInput.value);
             this.todoList.push(todoInputValue);
-            console.log(this.todoList);
+            localStorage.setItem("Todos", JSON.stringify(this.todoList));
             //Prepare the li element
             const todoItem = `<li>${todoInput === null || todoInput === void 0 ? void 0 : todoInput.value}</li>`;
             //Add it
--- typescript/TodoComponent.ts
+++ typescript/TodoComponent.ts
@@ -4,6 +4,8 @@ export class TodoComponent {
 
   public todoList: TodoModal[] = [];
   constructor(selector:string) {
+    //* Gets Todos from Local storage
+    this.todoList = JSON.parse(localStorage.getItem('Todos') ?? "[]");
     const template = `
         <div>
             <input type="text" id="todo-input" placeholder="Enter text here" />
@@ -20,11 +22,17 @@ export class TodoComponent {
     const itemList = document.querySelector("#item-list") as HTMLUListElement;
     const todoInput = document.querySelector("#todo-input") as HTMLInputElement;
 
+    //- Render the todoList on Dom
+    this.todoList.forEach((todo)=>{
+      const item = todo.todoItem;
+      itemList.insertAdjacentHTML('beforeend', `<li>${item}</li>`)
+    })
+
     addTodo.addEventListener("click", () => {
-      
+
       const todoInputValue = new TodoModal(todoInput?.value);
       this.todoList.push(todoInputValue);
-      console.log(this.todoList);
+      localStorage.setItem("Todos", JSON.stringify(this.todoList));
 
 
       //Prepare the li element
